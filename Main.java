e8806daf06a3a3911900700a5fa712e25ffbc2dc
razz3232 <74662119+razz3232@users.noreply.github.com>
2024-02-05 16:05:43 +0000

    output done

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Grades");
+        int grades = input.nextInt();
+
+        if(grades>=90){
+            System.out.println("A Grade");
+        }
+        else if(grades>79 || grades>=89){
+            System.out.println("B Grade");
+        }
+        else if(grades>69 || grades>=79){
+            System.out.println("C Grade");
+        }
+        else if(grades>59 || grades>=69){
+            System.out.println("D Grade");
+        }
+        else if(grades<=60){
+            System.out.println("F Grade");
+        }
+        else System.out.println("Invalid Grades");
+    }
+
+}
